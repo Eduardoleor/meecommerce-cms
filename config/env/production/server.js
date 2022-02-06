1d3a6c0b86ce284290794a2bc8c7803a92b9e5b9
eduardoleal <eduardo.leal.or@gmail.com>
2022-02-06 06:12:41 -0600

    fix! server

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,7 @@
 module.exports = ({ env }) => ({
   url: env('https://meecommerce-cms.herokuapp.com/'),
+  proxy: true,
+  app: {
+    keys: env.array("APP_KEYS", ["DATABASE_URL", "NODE_ENV", "meecommerce-cms"]),
+  },
 });
